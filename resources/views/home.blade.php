abbbd0aabcac6d58612455f29e1a598c7a673e72
Fauzan 320 <solihulfauzan@gmail.com>
2021-01-07 06:44:54 +0700

    coba background

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,6 +8,7 @@
     </div>
 </section>
 <!-- Hero section end --> --}}
+{{-- <img src="{{ url('template/img/page-top-bg.jpg') }}"> --}}
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-12 mb-4">
